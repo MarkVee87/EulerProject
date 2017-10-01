fba0e074b13ee077a59ac898984912e47fe5a911
Mark Vallis <1034528@my.brunel.ac.uk>
2017-10-02 00:13:41 +0100

    fibonnaci problem solved and class names refactored

--- src/main/java/Multiples3and5/Application.java
+++ src/main/java/Multiples3and5/Application.java
@@ -9,7 +9,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
+public class Application {
 
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();
