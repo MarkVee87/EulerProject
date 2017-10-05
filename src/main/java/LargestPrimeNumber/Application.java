e0a760154870b9c86d8e5a483f21323d3b8d7415
Mark Vallis <1034528@my.brunel.ac.uk>
2017-10-05 23:17:55 +0100

    prime factors using j8 needs optimising

--- src/main/java/LargestPrimeNumber/Application.java
+++ src/main/java/LargestPrimeNumber/Application.java
@@ -5,12 +5,11 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 
-itself and one
+answer: 6857
 */
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
 
 public class Application {
 
@@ -25,23 +24,20 @@ public class Application {
 
     private static List<Long> primeFactors(long number) {
 
-//         List<Long> factors = new ArrayList<>();
-//         for (long i = 2; i <= number; i++) {
-//             while (number % i == 0) {
-//                 factors.add(i);
-//                 number /= i;
-//             }
-//         }
-//        return factors;
-
-
-        List<Long> factors;
-        factors = LongStream.range(2, number)
-                .boxed()
-                .filter(x -> number % x == 0)
-                .collect(Collectors.toList());
+         List<Long> factors = new ArrayList<>();
+         for (long i = 2; i <= number; i++) {
+             while (number % i == 0) {
+                 factors.add(i);
+                 number /= i;
+             }
+         }
         return factors;
+
+
+//        Stream<Long> factors;
+//        factors = LongStream.range(2, number)
+//                .boxed()
+//                .filter(x -> number % x == 0);
+//        return factors;
     }
-
-
 }
