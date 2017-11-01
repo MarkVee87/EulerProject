e017b8e4fe87917166f77c6b91e5c1ebfbfccbbd
Mark Vallis <1034528@my.brunel.ac.uk>
2017-11-01 15:52:36 +0000

    finds highest palindromatic number as required, just needs to be converted into Java8

--- src/main/java/_4_LargestPalindroneProduct/Application.java
+++ src/main/java/_4_LargestPalindroneProduct/Application.java
@@ -4,6 +4,8 @@ package _4_LargestPalindroneProduct;
 A palindromic number reads the same both ways. The largest Palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 Find the largest Palindrome made from the product of two 3-digit numbers.
+
+906609
 */
 
 import org.junit.Test;
@@ -12,17 +14,26 @@ public class Application {
 
     public static void main(String[] args) {
 
-        long a, b;
+        int count = 0;
+        int highestPalindrome = 0;
+
+        int a, b, c;
         Palindrome palindrome = new Palindrome();
 
-        for (a = 111; a < 1000L; a++) {
-            for (b = 111; b < 1000L; b++) {
-                long c = a * b;
+        for (a = 111; a < 1000; a++) {
+            for (b = 111; b < 1000; b++) {
+                c = a * b;
                 if (palindrome.isPalindrome(c)) {
                     System.out.println(c);
+                    if(c > highestPalindrome){
+                        highestPalindrome = c;
+                    }
+                    count++;
                 }
             }
         }
+        System.out.println(count + " palindromes found");
+        System.out.println(highestPalindrome + " is the highest palindrome using the product of two three-digit numbers");
     }
 
     @Test
--- src/main/java/_4_LargestPalindroneProduct/Palindrome.java
+++ src/main/java/_4_LargestPalindroneProduct/Palindrome.java
@@ -7,32 +7,22 @@ class Palindrome {
     Palindrome() {
     }
 
-    boolean isPalindrome(long c) {
-
+    boolean isPalindrome(int c) {
         int i = String.valueOf(c).length();
         String value = String.valueOf(c);
-        String splitter;
-        String[] split;
+        int mid = value.length() / 2;
 
         if (i % 2 == 0) {
-            int mid = value.length() / 2;
             String[] parts = {value.substring(0, mid),value.substring(mid)};
-            return checkSides(parts, c);
+            return checkSides(parts);
         } else {
-            splitter = String.valueOf(value.charAt(i / 2));
-            split = value.split(splitter);
-            return checkSides(split, c);
+            String[] parts = {value.substring(0, mid),value.substring(mid)};
+            return checkSides(parts);
         }
     }
 
-    private boolean checkSides(String[] parts, long c){
-        if (Objects.equals(parts[0], new StringBuilder(parts[1]).reverse().toString())) {
-            System.out.println(c + " is a Palindrome");
-            return true;
-        } else {
-            System.out.println(c + " is not a Palindrome");
-            return false;
-        }
+    private boolean checkSides(String[] parts){
+        return Objects.equals(parts[0], new StringBuilder(parts[1]).reverse().toString());
     }
 
 }
