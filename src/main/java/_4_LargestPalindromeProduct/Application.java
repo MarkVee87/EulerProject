e174c71e7235ccc5dd19ec7ac29950547fa599b9
Mark Vallis <1034528@my.brunel.ac.uk>
2017-11-01 18:27:47 +0000

    Reordered methods

--- src/main/java/_4_LargestPalindromeProduct/Application.java
+++ src/main/java/_4_LargestPalindromeProduct/Application.java
@@ -16,26 +16,36 @@ public class Application {
     private static long newJavaDuration;
 
     public static void main(String[] args) {
-        yeOldeJava();
         java8Stream();
+        yeOldeJava();
         findFastest();
     }
 
-    private static void findFastest() {
+    private static void java8Stream() {
+        long start = System.currentTimeMillis();
+        final int[] count = {0};
+        final int[] highestCurrentPalindrome = {0};
 
-        System.out.println("\n" + "Pre-Java8 time (ms): " + oldJavaDuration);
-        System.out.println("Java8 time (ms) " + newJavaDuration);
-        long diff, percentDiff;
+        Palindrome palindrome = new Palindrome();
 
-        if (oldJavaDuration > newJavaDuration) {
-            diff = oldJavaDuration - newJavaDuration;
-            percentDiff = (long) ((float) oldJavaDuration / newJavaDuration) * 100;
-            System.out.println("Java8 method was faster by " + diff + "ms, " + percentDiff + "%");
-        } else {
-            diff = newJavaDuration - oldJavaDuration;
-            percentDiff = (long) (((float) newJavaDuration / oldJavaDuration) * 100);
-            System.out.println("Pre-Java8 method was faster by " + diff + "ms, " + percentDiff + "%");
-        }
+        IntStream.rangeClosed(100, 999)
+                .forEach(x -> IntStream.rangeClosed(100, 999)
+                        .forEach(p -> {
+                            final int c = x * p;
+                            if (palindrome.isPalindrome(c)) {
+                                System.out.println(c);
+                                if (c > highestCurrentPalindrome[0]) {
+                                    highestCurrentPalindrome[0] = c;
+                                }
+                                count[0]++;
+                            }
+                        }));
+
+        long end = System.currentTimeMillis();
+        newJavaDuration = end - start;
+
+        System.out.println("\n" + count[0] + " palindromes found");
+        System.out.println(highestCurrentPalindrome[0] + " is the highest palindrome using the product of two three-digit numbers");
     }
 
     private static void yeOldeJava() {
@@ -65,30 +75,20 @@ public class Application {
         System.out.println(highestCurrentPalindrome + " is the highest palindrome using the product of two three-digit numbers");
     }
 
-    private static void java8Stream() {
-        long start = System.currentTimeMillis();
-        final int[] count = {0};
-        final int[] highestCurrentPalindrome = {0};
+    private static void findFastest() {
 
-        Palindrome palindrome = new Palindrome();
+        System.out.println("\n" + "Pre-Java8 time (ms): " + oldJavaDuration);
+        System.out.println("Java8 time (ms) " + newJavaDuration);
+        long diff, percentDiff;
 
-        IntStream.rangeClosed(100, 999)
-                .forEach(x -> IntStream.rangeClosed(100, 999)
-                        .forEach(p -> {
-                            final int c = x * p;
-                            if (palindrome.isPalindrome(c)) {
-                                System.out.println(c);
-                                if (c > highestCurrentPalindrome[0]) {
-                                    highestCurrentPalindrome[0] = c;
-                                }
-                                count[0]++;
-                            }
-                        }));
-
-        long end = System.currentTimeMillis();
-        newJavaDuration = end - start;
-
-        System.out.println("\n" + count[0] + " palindromes found");
-        System.out.println(highestCurrentPalindrome[0] + " is the highest palindrome using the product of two three-digit numbers");
+        if (oldJavaDuration > newJavaDuration) {
+            diff = oldJavaDuration - newJavaDuration;
+            percentDiff = (long) ((float) oldJavaDuration / newJavaDuration) * 100;
+            System.out.println("Java8 method was faster by " + diff + "ms, " + percentDiff + "%");
+        } else {
+            diff = newJavaDuration - oldJavaDuration;
+            percentDiff = (long) (((float) newJavaDuration / oldJavaDuration) * 100);
+            System.out.println("Pre-Java8 method was faster by " + diff + "ms, " + percentDiff + "%");
+        }
     }
 }
