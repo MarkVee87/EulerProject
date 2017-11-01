121200d2be768cbe4da8658f65e565477d1d7c03
Mark Vallis <1034528@my.brunel.ac.uk>
2017-11-01 17:23:57 +0000

    Converted palindrome example to Java8 Stream

--- src/main/java/_4_LargestPalindromeProduct/Application.java
+++ src/main/java/_4_LargestPalindromeProduct/Application.java
@@ -8,31 +8,29 @@ Find the largest Palindrome made from the product of two 3-digit numbers.
 906609
 */
 
-import org.junit.Test;
-
 import java.util.stream.IntStream;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        int count = 0;
+        yeOldeJava();
+
+        java8Stream();
+    }
+
+    private static void yeOldeJava(){
+        int a, b;
         int highestCurrentPalindrome = 0;
-
-        int a, b, c;
+        int count = 0;
         Palindrome palindrome = new Palindrome();
-
-//        TODO: convert to Java8 stream
-
-
-
 
         for (a = 100; a < 1000; a++) {
             for (b = 100; b < 1000; b++) {
-                c = a * b;
+                int c = a * b;
                 if (palindrome.isPalindrome(c)) {
                     System.out.println(c);
-                    if(c > highestCurrentPalindrome){
+                    if (c > highestCurrentPalindrome) {
                         highestCurrentPalindrome = c;
                     }
                     count++;
@@ -43,12 +41,27 @@ public class Application {
         System.out.println(highestCurrentPalindrome + " is the highest palindrome using the product of two three-digit numbers");
     }
 
+    private static void java8Stream(){
+        final int[] count = {0};
+        final int[] highestCurrentPalindrome = {0};
 
-    @Test
-    public void streamTest(){
-        int i = 1000;
-        IntStream.range(0, i)
-                .forEach(System.out::println);
+        Palindrome palindrome = new Palindrome();
+
+        IntStream.rangeClosed(100, 999)
+                .forEach(x -> IntStream.rangeClosed(100, 999)
+                        .forEach(
+                                p -> {
+                                    final int c = x * p;
+                                    if (palindrome.isPalindrome(c)) {
+                                        System.out.println(c);
+                                        if (c > highestCurrentPalindrome[0]) {
+                                            highestCurrentPalindrome[0] = c;
+                                        }
+                                        count[0]++;
+                                    }
+                                }));
+
+        System.out.println(count[0] + " palindromes found");
+        System.out.println(highestCurrentPalindrome[0] + " is the highest palindrome using the product of two three-digit numbers");
     }
-
 }
